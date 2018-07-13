4c658d91a5d0414eaffb2e29760e78d7dec6df37
rosso4400 <aleksey_shusharin@mail.ru>
2018-07-13 16:28:36 +0300

    redacting without validation

--- catalog/views.py
+++ catalog/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.http import HttpResponseNotFound
-from django.views.generic.edit import UpdateView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Book, Author
 
@@ -37,11 +37,31 @@ class AuthorDetailView(LoginRequiredMixin, generic.DetailView):
 class BookUpdate(LoginRequiredMixin, UpdateView):
     model = Book
     fields = ['book_title', 'author']
-    template_name_suffix = '_update'
+    #template_name_suffix = '_update'
 
 
 class BookCreate(LoginRequiredMixin, CreateView):
     model = Book
     fields = ['book_title', 'author']
+
+
+class BookDelete(LoginRequiredMixin, DeleteView):
+    model = Book
+    success_url = reverse_lazy('books')
+
+
+class AuthorCreate(LoginRequiredMixin, CreateView):
+    model = Author
+    fields = '__all__'
+
+
+class AuthorUpdate(LoginRequiredMixin, UpdateView):
+    model = Author
+    fields = ['author_name']
+
+
+class AuthorDelete(LoginRequiredMixin, DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
 
 
